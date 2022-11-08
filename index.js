7c5bdbd7bfe9da14c602b7c9626b168a62655f80
Your Name <you@example.com>
2022-11-07 20:08:49 -0800

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// Require needed modules.
+require('dotenv').config()
 const express = require('express')
-
-// Initialize the app object.
 const app = express()
 
-// Create a homepage route.
-app.get('/', function (req, res) {
-    // This gets sent to the client
-    // (your web browser most likely!)
-    res.send('Hello world')
+app.get('/', (req, res) => {
+    res.send('Hello world!')
 })
 
-// Listen for connections.
-app.listen(3002)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
