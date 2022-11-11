3f385d5acacc28a3b696e1989ef600e6d630b28d
Your Name <you@example.com>
2022-11-10 21:17:18 -0800

    changes to code for part 4

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,9 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+      <img src="/images/error-cat.jpg" alt="error cat" height="100px" width="100px"/>
+      </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -4,12 +4,20 @@ const Def = require('./default.jsx')
 function home () {
     return (
       <Def>
-          <main>
-              <h1>HOME</h1>
-              </main>
-              <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+<main>
+  
+    <h1>HOME</h1>
+    <div>
+      <img src="/images/chia-snack.jpg" alt="Chia Fruit Shake" height="100px" width="100px"/>
+      <div>
+        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+      </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
+
 
       </Def>
     )
