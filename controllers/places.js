6dd5b88d71ee7e261b3c09bc77930178ccac8ca0
Your Name <you@example.com>
2022-11-11 21:16:33 -0800

    Adding places view with mock data

--- controllers/places.js
+++ controllers/places.js
@@ -10,15 +10,16 @@ router.get('/', (req, res) => {
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
+    pic: 'https://placekitten.com/250/250'
   }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
+    pic: 'https://placekitten.com/250/250'
   }]
-  res.render('places/index')
+  res.render('places/index', { places })
+
 
 })
 
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,15 +2,25 @@ const React = require('react')
 const Def = require('../default.jsx')
 
 
-function index () {
+function index (data) {
+  let placesFormatted = data.places.map((place) => {
     return (
-      <Def>
-          <main>
-              <h1>PLACES</h1>
-          </main>
-      </Def>
+      <div>
+        <h2>{place.name}</h2>
+        <img src={place.pic} alt={place.name}/>
+      </div>
     )
-  }
+  })
+  return (
+    <Def>
+        <main>
+            <h1>PLACES INDEX PAGE</h1>
+            {placesFormatted}
+        </main>
+    </Def>
+)
+}
+
 
 
 module.exports = index
