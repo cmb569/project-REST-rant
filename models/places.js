3922908016b0a431f022a1ab2dd2eac583b8012a
Your Name <you@example.com>
2022-11-17 19:49:05 -0800

    Changed everything into mongoose method

--- models/places.js
+++ models/places.js
@@ -1,4 +1,23 @@
-module.exports = [{
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+
+
+
+////Previous coding:////
+
+/*module.exports = [{
     name: 'H-Thai-ML',
     city: 'Seattle',
     state: 'WA',
@@ -10,4 +29,4 @@ module.exports = [{
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
     pic: '/images/cat-cafe.jpg'
-}]
+}]*/
