6521d969d9a10d556faf674ae45807f6ce64b2f8
Your Name <you@example.com>
2022-11-20 15:19:47 -0800

    Added code to show.jsx

--- models/places.js
+++ models/places.js
@@ -6,10 +6,16 @@ const placeSchema = new mongoose.Schema({
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
-  founded: {Number}
+  founded: Number
 })
 
+placeSchema.methods.showEstablished = function() {
+    return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+  }
+  
+
 module.exports = mongoose.model('Place', placeSchema)
+
 
 
 
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,7 +9,24 @@ function show (data)
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
+          <div className="row">
+      <div className="col-sm-6">
+        <img src={data.place.pic} alt={data.place.name} />
+        <h3>
+          Located in {data.place.city}, {data.place.state}
+        </h3>
+      </div>
+      <div className="col-sm-6">
+      <h1>{ data.place.name }</h1>
+      <h2>
+          Description
+        </h2>
+        <h3>
+          {data.place.showEstablished()}
+        </h3>
+        <h4>
+          Serving {data.place.cuisines}
+        </h4>
             <h2>Rating</h2>
             <p>Not Rated</p>
             <h2>Description</h2>
@@ -21,12 +38,13 @@ function show (data)
   <button type="submit" className="btn btn-danger">
     Delete
   </button>
-</form> 
-
-          
-    
-           </main>
-        </Def>
+  
+</form>
+      </div>
+      
+</div> 
+</main>
+ </Def>
     )
 }
 
